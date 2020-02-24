07207fa3ae4358dd210908972355d421790f307b
DELL <3601113109@qq.com>
2020-02-24 17:30:00 +0800

    first jacrt store back

---
+++ jacrt-store-back/src/main/java/io/lkr/jacrtstoreback/JacrtStoreBackApplication.java
@@ -0,0 +1,13 @@
+package io.lkr.jacrtstoreback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacrtStoreBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacrtStoreBackApplication.class, args);
+    }
+
+}
---
+++ jacrt-store-back/src/test/java/io/lkr/jacrtstoreback/JacrtStoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lkr.jacrtstoreback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacrtStoreBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
