72d300d5493970767d396eb37d9ff00b4cb70090
DELL <3601113109@qq.com>
2020-02-24 17:27:18 +0800

    first init jcart adminstration back

---
+++ jacrt-administration-back/src/main/java/io/lkr/jacrtadministrationback/JacrtAdministrationBackApplication.java
@@ -0,0 +1,13 @@
+package io.lkr.jacrtadministrationback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacrtAdministrationBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacrtAdministrationBackApplication.class, args);
+    }
+
+}
---
+++ jacrt-administration-back/src/test/java/io/lkr/jacrtadministrationback/JacrtAdministrationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lkr.jacrtadministrationback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacrtAdministrationBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
