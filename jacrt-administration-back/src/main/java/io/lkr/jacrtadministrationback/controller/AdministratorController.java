f2f88a858d0f20592463a494d6e4111fb6a30fc5
DELL <3601113109@qq.com>
2020-02-25 15:34:02 +0800

    draft some api

--- jacrt-administration-back/src/main/java/io/lkr/jacrtadministrationback/controller/AdministratorController.java
+++ jacrt-administration-back/src/main/java/io/lkr/jacrtadministrationback/controller/AdministratorController.java
@@ -1,10 +1,9 @@
 package io.lkr.jacrtadministrationback.controller;
 
 import io.lkr.jacrtadministrationback.dto.in.AdministratorLoginInDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.lkr.jacrtadministrationback.dto.in.AdministratorUpdateProfileInDTO;
+import io.lkr.jacrtadministrationback.dto.out.AdministratorGetProfileOutDTO;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/administrator")
@@ -16,9 +15,14 @@ public class AdministratorController {
     }
 
 
+    @GetMapping("/getProfile")
+    public AdministratorGetProfileOutDTO getProfile(Integer administratorId){
+
+        return null;
+    }
 
     @PostMapping("/updateProdfile")
-    public void updateProdfile(){
+    public void updateProdfile(@RequestBody AdministratorUpdateProfileInDTO administratorUpdateProfileInDTO){
 
     }
 
